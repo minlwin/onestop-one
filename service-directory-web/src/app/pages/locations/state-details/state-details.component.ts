c52479573470b631890bafcadb0c482b1d884e80
Min Lwin <lwin.zawmin@gmail.com>
2022-11-25 11:11:25 +0630

    Add State Info Component

--- service-directory-web/src/app/pages/locations/state-details/state-details.component.ts
+++ service-directory-web/src/app/pages/locations/state-details/state-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StateService } from 'src/app/services/state.service';
 
 @Component({
   templateUrl: './state-details.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class StateDetailsComponent {
 
+  targetState:any
+
+  constructor(route:ActivatedRoute, stateApi:StateService) {
+    route.queryParamMap.subscribe(params => {
+      const id = params.get('id')
+      stateApi.findById(id).subscribe(result => this.targetState = result)
+    })
+  }
 }
--- service-directory-web/src/app/pages/locations/state-details/state-info/state-info.component.ts
+++ service-directory-web/src/app/pages/locations/state-details/state-info/state-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-state-info',
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class StateInfoComponent {
 
+  @Input()
+  data:any
 }
